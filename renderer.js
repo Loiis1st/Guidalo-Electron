e502498270eaf9895594891d9147ea705ce67ba5
Loiis <92925696+Loiis1st@users.noreply.github.com>
2022-05-12 21:47:14 +0400

    Add files via upload

---
+++ renderer.js
@@ -0,0 +1,5 @@
+//Make Node.js API calls in this file
+
+ let navbar = '<div class="container"><a class="navbar-brand" href="index.html"><i class="bi bi-house-door"></i> Accueil</a><a class="navbar-brand" href="profil.html"><i class="bi bi-person-circle"></i> Profil</a><a class="navbar-brand" href="etudiant.html"><i class="bi bi-people"></i> Etudiant</a><a class="navbar-brand" href="personnel.html"><i class="bi bi-person-video2"></i> Personnel</a></div>';
+
+document.getElementById('navbar').innerHTML = navbar;
